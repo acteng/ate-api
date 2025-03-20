aee245aefb534004e2d698f326fb291a98626824
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-20 16:50:25 +0000

    GH-1: Pull up IT fixtures to package

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,13 +1,50 @@
+from typing import Generator
+
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
+from ate_api.authorities import (
+    AuthorityRepository,
+    MemoryAuthorityRepository,
+    get_authority_repository,
+)
 from ate_api.main import app
+from ate_api.settings import Settings, get_settings
+from tests.integration.oauth import StubAuthorizationServer
+
+
+@pytest.fixture(name="resource_server_identifier", scope="package")
+def resource_server_identifier_fixture() -> str:
+    return "https://api.example"
+
+
+@pytest.fixture(name="authorization_server", scope="package")
+def authorization_server_fixture(resource_server_identifier: str) -> StubAuthorizationServer:
+    authorization_server = StubAuthorizationServer(resource_server_identifier)
+    authorization_server.given_configuration_endpoint_returns_configuration()
+    return authorization_server
+
+
+@pytest.fixture(name="settings")
+def settings_fixture(authorization_server: StubAuthorizationServer, resource_server_identifier: str) -> Settings:
+    return Settings(
+        oidc_server_metadata_url=authorization_server.configuration_endpoint,
+        resource_server_identifier=resource_server_identifier,
+    )
+
+
+@pytest.fixture(name="authorities")
+def authorities_fixture() -> AuthorityRepository:
+    return MemoryAuthorityRepository()
 
 
 @pytest.fixture(name="app")
-def app_fixture() -> FastAPI:
-    return app
+def app_fixture(settings: Settings, authorities: AuthorityRepository) -> Generator[FastAPI, None, None]:
+    app.dependency_overrides[get_settings] = lambda: settings
+    app.dependency_overrides[get_authority_repository] = lambda: authorities
+    yield app
+    app.dependency_overrides = {}
 
 
 @pytest.fixture(name="client")
--- tests/integration/test_authorities.py
+++ tests/integration/test_authorities.py
@@ -1,52 +1,10 @@
 from datetime import datetime
-from typing import Generator
 
-import pytest
 import respx
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from ate_api.authorities import (
-    Authority,
-    AuthorityRepository,
-    MemoryAuthorityRepository,
-    get_authority_repository,
-)
-from ate_api.settings import Settings, get_settings
+from ate_api.authorities import Authority, AuthorityRepository
 from tests.integration.oauth import StubAuthorizationServer
-
-
-@pytest.fixture(name="resource_server_identifier", scope="module")
-def resource_server_identifier_fixture() -> str:
-    return "https://api.example"
-
-
-@pytest.fixture(name="authorization_server", scope="module")
-def authorization_server_fixture(resource_server_identifier: str) -> StubAuthorizationServer:
-    authorization_server = StubAuthorizationServer(resource_server_identifier)
-    authorization_server.given_configuration_endpoint_returns_configuration()
-    return authorization_server
-
-
-@pytest.fixture(name="settings")
-def settings_fixture(authorization_server: StubAuthorizationServer, resource_server_identifier: str) -> Settings:
-    return Settings(
-        oidc_server_metadata_url=authorization_server.configuration_endpoint,
-        resource_server_identifier=resource_server_identifier,
-    )
-
-
-@pytest.fixture(name="authorities")
-def authorities_fixture() -> AuthorityRepository:
-    return MemoryAuthorityRepository()
-
-
-@pytest.fixture(name="app")
-def app_fixture(app: FastAPI, settings: Settings, authorities: AuthorityRepository) -> Generator[FastAPI, None, None]:
-    app.dependency_overrides[get_settings] = lambda: settings
-    app.dependency_overrides[get_authority_repository] = lambda: authorities
-    yield app
-    app.dependency_overrides = {}
 
 
 @respx.mock
