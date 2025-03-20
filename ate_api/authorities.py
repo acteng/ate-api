a12daaa4c290b5576322db44ab0bc20413a340cd
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-20 11:44:39 +0000

    GH-1: Introduce authority model

--- ate_api/authorities.py
+++ ate_api/authorities.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
+from pydantic import AliasGenerator, BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 router = APIRouter()
 
 
+class AuthorityModel(BaseModel):
+    abbreviation: str
+    full_name: str
+
+    model_config = ConfigDict(alias_generator=AliasGenerator(serialization_alias=to_camel))
+
+
 @router.get("/authorities/{authority_abbreviation}")
-async def get_authority(authority_abbreviation: str) -> dict[str, str]:
-    return {"abbreviation": "LIV", "fullName": "Liverpool City Region Combined Authority"}
+async def get_authority(authority_abbreviation: str) -> AuthorityModel:
+    return AuthorityModel(abbreviation="LIV", full_name="Liverpool City Region Combined Authority")
