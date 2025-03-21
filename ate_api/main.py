05fdb5d8738027bc3136cc7924df0872648d7a8c
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-21 18:00:52 +0000

    GH-1: Document 403 for all endpoints

--- ate_api/main.py
+++ ate_api/main.py
@@ -1,11 +1,19 @@
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from starlette.status import HTTP_403_FORBIDDEN
 
 from ate_api import authorities
 from ate_api.auth import authorize
+
+
+class HTTPError(BaseModel):
+    detail: str
+
 
 app = FastAPI(
     title="ATE API",
     description="The Active Travel England Web API.",
     dependencies=[Depends(authorize)],
+    responses={HTTP_403_FORBIDDEN: {"model": HTTPError}},
 )
 app.include_router(authorities.router)
