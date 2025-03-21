38e3ac9bddeeb86821a387389b89c5f609337406
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-21 16:10:40 +0000

    GH-1: Correct test API responses to have no content

--- tests/e2e/routes.py
+++ tests/e2e/routes.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from fastapi.params import Depends
 from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
 
@@ -15,13 +15,16 @@ from ate_api.authorities import (
 router = APIRouter()
 
 
-@router.post("/test/authorities", status_code=HTTP_201_CREATED)
+@router.post("/test/authorities", status_code=HTTP_201_CREATED, response_class=Response)
 async def create_authority(
     authorities: Annotated[AuthorityRepository, Depends(get_authority_repository)], authority: AuthorityModel
 ) -> None:
     authorities.add(authority.to_domain())
 
 
-@router.delete("/test/authorities", status_code=HTTP_204_NO_CONTENT, response_model=None)
-async def delete_authorities(authorities: Annotated[AuthorityRepository, Depends(get_authority_repository)]) -> None:
+@router.delete("/test/authorities", status_code=HTTP_204_NO_CONTENT)
+async def delete_authorities(
+    authorities: Annotated[AuthorityRepository, Depends(get_authority_repository)],
+) -> Response:
     authorities.clear()
+    return Response(status_code=HTTP_204_NO_CONTENT)
