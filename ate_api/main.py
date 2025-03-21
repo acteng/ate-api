77706c16c3bcfa9828b0f32d7d0691173ff607b5
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-21 17:22:38 +0000

    GH-1: Add API metadata

--- ate_api/main.py
+++ ate_api/main.py
@@ -3,5 +3,9 @@ from fastapi import Depends, FastAPI
 from ate_api import authorities
 from ate_api.auth import authorize
 
-app = FastAPI(dependencies=[Depends(authorize)])
+app = FastAPI(
+    title="ATE API",
+    description="The Active Travel England Web API.",
+    dependencies=[Depends(authorize)],
+)
 app.include_router(authorities.router)
