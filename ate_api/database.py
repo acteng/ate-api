07fc21480f4e64af35c528f49c8232ea5b2672d6
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-03-25 12:41:55 +0000

    GH-1: Run locally using Compose

--- ate_api/database.py
+++ ate_api/database.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 from typing import Annotated, Generator
 
 from fastapi.params import Depends
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine, create_engine, text
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.ddl import CreateSchema
 
@@ -14,7 +14,7 @@ from ate_api.settings import Settings, get_settings
 def get_engine(settings: Annotated[Settings, Depends(get_settings)]) -> Engine:
     engine = create_engine(settings.database_url)
 
-    if settings.create_database_schema:
+    if settings.create_database_schema and not _schema_exists(engine):
         _create_schema(engine)
 
     return engine
@@ -23,6 +23,17 @@ def get_engine(settings: Annotated[Settings, Depends(get_settings)]) -> Engine:
 def get_session(engine: Annotated[Engine, Depends(get_engine)]) -> Generator[Session]:
     with Session(engine) as session:
         yield session
+
+
+def _schema_exists(engine: Engine) -> bool:
+    with engine.begin() as connection:
+        result = connection.execute(
+            text(
+                "SELECT EXISTS(SELECT schema_name FROM information_schema.schemata WHERE schema_name = :schema_name);"
+            ),
+            {"schema_name": "authority"},
+        )
+        return bool(result.scalar_one())
 
 
 def _create_schema(engine: Engine) -> None:
