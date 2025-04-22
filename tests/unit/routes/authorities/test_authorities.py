3853026b5aea8472a2b2cf48bab75df62438c587
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-04-22 16:32:56 +0100

    Use object equality for model classes

--- tests/unit/routes/authorities/test_authorities.py
+++ tests/unit/routes/authorities/test_authorities.py
@@ -18,10 +18,10 @@ class TestAuthorityModel:
 
         authority_model = AuthorityModel.from_domain(authority, app)
 
-        assert (
-            authority_model.abbreviation == "LIV"
-            and authority_model.full_name == "Liverpool City Region Combined Authority"
-            and authority_model.bid_submitting_capital_schemes == "/authorities/LIV/capital-schemes/bid-submitting"
+        assert authority_model == AuthorityModel(
+            abbreviation="LIV",
+            full_name="Liverpool City Region Combined Authority",
+            bid_submitting_capital_schemes="/authorities/LIV/capital-schemes/bid-submitting",
         )
 
     def test_to_domain(self) -> None:
