0cfbd763da186b38aa1bc63337855f34363b3889
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-05-20 12:36:39 +0100

    Ensure to-date equal to or after from-date in date time range

--- ate_api/domain/dates.py
+++ ate_api/domain/dates.py
@@ -6,3 +6,7 @@ from datetime import datetime
 class DateTimeRange:
     from_: datetime
     to: datetime | None = None
+
+    def __post_init__(self) -> None:
+        if not (self.to is None or self.from_ <= self.to):
+            raise ValueError(f"From '{self.from_}' must not be after to '{self.to}'")
--- tests/unit/domain/test_dates.py
+++ tests/unit/domain/test_dates.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import pytest
+
 from ate_api.domain.dates import DateTimeRange
 
 
@@ -13,3 +15,18 @@ class TestDateTimeRange:
         date_range = DateTimeRange(datetime(2020, 1, 1))
 
         assert date_range.from_ == datetime(2020, 1, 1) and not date_range.to
+
+    @pytest.mark.parametrize(
+        "from_, to",
+        [
+            (datetime(2020, 1, 1), datetime(2020, 2, 1)),
+            (datetime(2020, 2, 1), datetime(2020, 2, 1)),
+            (datetime(2020, 1, 1), None),
+        ],
+    )
+    def test_from_before_or_equal_to_to(self, from_: datetime, to: datetime | None) -> None:
+        DateTimeRange(from_, to)
+
+    def test_from_after_to(self) -> None:
+        with pytest.raises(ValueError, match="From '2020-01-01 12:00:00' must not be after to '2019-12-31 13:00:00'"):
+            DateTimeRange(datetime(2020, 1, 1, 12), datetime(2019, 12, 31, 13))
