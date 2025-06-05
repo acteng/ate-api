69039f82d68f4091794b893ba77e343304877744
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-06-05 17:14:05 +0100

    Upgrade Authlib to 1.6.x

--- tests/e2e/oauth/servers.py
+++ tests/e2e/oauth/servers.py
@@ -3,9 +3,26 @@ from typing import Any, Callable
 
 from authlib.oauth2 import AuthorizationServer, OAuth2Request
 from authlib.oauth2.rfc6749 import ClientMixin
-from starlette.datastructures import FormData
+from authlib.oauth2.rfc6749.requests import BasicOAuth2Payload
+from starlette.datastructures import FormData, UploadFile
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
+
+
+class StarletteOAuth2Request(OAuth2Request):  # type: ignore
+    def __init__(self, request: Request):
+        super().__init__(request.method, str(request.url), request.headers)
+        self._form = asyncio.run(self._get_form(request))
+        self.payload = BasicOAuth2Payload(self._form)
+
+    @property
+    def form(self) -> dict[str, UploadFile | str]:
+        return dict(self._form)
+
+    @staticmethod
+    async def _get_form(request: Request) -> FormData:
+        async with request.form() as form:
+            return form
 
 
 class StarletteAuthorizationServer(AuthorizationServer):  # type: ignore
@@ -25,13 +42,7 @@ class StarletteAuthorizationServer(AuthorizationServer):  # type: ignore
         pass
 
     def create_oauth2_request(self, request: Request) -> OAuth2Request:
-        form = asyncio.run(self._get_form(request))
-        return OAuth2Request(request.method, str(request.url), dict(form), request.headers)
+        return StarletteOAuth2Request(request)
 
     def handle_response(self, status: int, body: Any, headers: list[tuple[str, str]]) -> Response:
         return JSONResponse(body, status, dict(headers))
-
-    @staticmethod
-    async def _get_form(request: Request) -> FormData:
-        async with request.form() as form:
-            return form
