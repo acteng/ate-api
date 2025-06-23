fd1e0cd51fcd644c5f1ca346717e653f7c62eefc
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-06-23 15:37:22 +0100

    GH-46: Enable latest preconfigured WAF ruleset

--- cloud/service/web-application-firewall/main.tf
+++ cloud/service/web-application-firewall/main.tf
@@ -1,3 +1,114 @@
 resource "google_compute_security_policy" "ate_api" {
   name = "ate-api"
+
+  rule {
+    description = "SQL injection"
+    action      = "deny(403)"
+    priority    = 1000
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('sqli-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Cross-site scripting"
+    action      = "deny(403)"
+    priority    = 1001
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('xss-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Local file inclusion"
+    action      = "deny(403)"
+    priority    = 1002
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('lfi-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Remote file inclusion"
+    action      = "deny(403)"
+    priority    = 1003
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('rfi-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Remote code execution"
+    action      = "deny(403)"
+    priority    = 1004
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('rce-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Method enforcement"
+    action      = "deny(403)"
+    priority    = 1005
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('methodenforcement-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Scanner detection"
+    action      = "deny(403)"
+    priority    = 1006
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('scannerdetection-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Protocol attack"
+    action      = "deny(403)"
+    priority    = 1007
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('protocolattack-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "Session fixation attack"
+    action      = "deny(403)"
+    priority    = 1008
+    match {
+      expr {
+        expression = "evaluatePreconfiguredWaf('sessionfixation-v33-stable', {'sensitivity': 1})"
+      }
+    }
+  }
+
+  rule {
+    description = "default rule"
+    action      = "allow"
+    priority    = 2147483647
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+  }
 }
