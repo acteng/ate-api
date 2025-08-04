d55f8a4a05d4741e6acf01ba706d2e95a9d21001
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-08-04 18:14:43 +0100

    WIP: Attempt to use JWTBearerTokenValidator directly
    Places more constraints on the JWT claims which needs investigation.

--- ate_api/auth.py
+++ ate_api/auth.py
@@ -2,14 +2,25 @@ from functools import lru_cache
 from typing import Annotated
 
 from authlib.integrations.starlette_client import OAuth
-from authlib.jose import jwt
-from authlib.jose.errors import ExpiredTokenError, InvalidClaimError, InvalidTokenError
+from authlib.jose import KeySet
+from authlib.jose.errors import BadSignatureError
+from authlib.oauth2.rfc6750 import InvalidTokenError
+from authlib.oauth2.rfc9068 import JWTBearerTokenValidator
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from ate_api.settings import Settings, get_settings
 
 bearer_scheme = HTTPBearer(bearerFormat="JWT")
+
+
+class MyJWTBearerTokenValidator(JWTBearerTokenValidator):  # type: ignore
+    def __init__(self, issuer: str, resource_server: str, jwks: KeySet):
+        super().__init__(issuer, resource_server)
+        self._jwks = jwks
+
+    def get_jwks(self) -> KeySet:
+        return self._jwks
 
 
 @lru_cache
@@ -28,21 +39,18 @@ async def authorize(
     server_metadata = await oauth.auth0.load_server_metadata()
     jwks = await oauth.auth0.fetch_jwk_set()
 
+    token_validator = MyJWTBearerTokenValidator(
+        issuer=server_metadata.get("issuer"), resource_server=settings.resource_server_identifier, jwks=jwks
+    )
+
     # validate signature
     try:
-        claims = jwt.decode(
-            token,
-            key=jwks,
-            claims_options={
-                "iss": {"value": server_metadata.get("issuer")},
-                "aud": {"value": settings.resource_server_identifier},
-            },
-        )
-    except Exception as error:
+        claims = token_validator.authenticate_token(token)
+    except BadSignatureError as error:
         raise HTTPException(status.HTTP_403_FORBIDDEN, str(error))
 
     # validate claims
     try:
-        claims.validate()
-    except (InvalidClaimError, ExpiredTokenError, InvalidTokenError) as error:
+        token_validator.validate_token(claims, scopes=None, request=None)
+    except InvalidTokenError as error:
         raise HTTPException(status.HTTP_403_FORBIDDEN, str(error))
