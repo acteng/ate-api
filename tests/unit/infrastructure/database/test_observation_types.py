b2020bf12b319cc8d8ee2ab9e317d5f2417b15f1
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-25 12:43:12 +0100

    Ensure all observation types can be mapped

--- tests/unit/infrastructure/database/test_observation_types.py
+++ tests/unit/infrastructure/database/test_observation_types.py
@@ -1,10 +1,19 @@
+import pytest
+
 from ate_api.domain.observation_types import ObservationType
 from ate_api.infrastructure.database import ObservationTypeName
 
 
+@pytest.mark.parametrize(
+    "type_, type_name",
+    [
+        (ObservationType.PLANNED, ObservationTypeName.PLANNED),
+        (ObservationType.ACTUAL, ObservationTypeName.ACTUAL),
+    ],
+)
 class TestObservationTypeName:
-    def test_from_domain(self) -> None:
-        assert ObservationTypeName.from_domain(ObservationType.ACTUAL) == ObservationTypeName.ACTUAL
+    def test_from_domain(self, type_: ObservationType, type_name: ObservationTypeName) -> None:
+        assert ObservationTypeName.from_domain(type_) == type_name
 
-    def test_to_domain(self) -> None:
-        assert ObservationTypeName.ACTUAL.to_domain() == ObservationType.ACTUAL
+    def test_to_domain(self, type_: ObservationType, type_name: ObservationTypeName) -> None:
+        assert type_name.to_domain() == type_
--- tests/unit/routes/test_observation_types.py
+++ tests/unit/routes/test_observation_types.py
@@ -1,10 +1,19 @@
+import pytest
+
 from ate_api.domain.observation_types import ObservationType
 from ate_api.routes.observation_types import ObservationTypeModel
 
 
+@pytest.mark.parametrize(
+    "type_, type_model",
+    [
+        (ObservationType.PLANNED, ObservationTypeModel.PLANNED),
+        (ObservationType.ACTUAL, ObservationTypeModel.ACTUAL),
+    ],
+)
 class TestObservationTypeModel:
-    def test_from_domain(self) -> None:
-        assert ObservationTypeModel.from_domain(ObservationType.ACTUAL) == ObservationTypeModel.ACTUAL
+    def test_from_domain(self, type_: ObservationType, type_model: ObservationTypeModel) -> None:
+        assert ObservationTypeModel.from_domain(type_) == type_model
 
-    def test_to_domain(self) -> None:
-        assert ObservationTypeModel.ACTUAL.to_domain() == ObservationType.ACTUAL
+    def test_to_domain(self, type_: ObservationType, type_model: ObservationTypeModel) -> None:
+        assert type_model.to_domain() == type_
