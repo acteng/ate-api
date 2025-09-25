aa5420a30f17ce5f9d0ad6c2d21840511610031f
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-25 12:43:42 +0100

    Ensure all financial types can be mapped

--- tests/unit/infrastructure/database/test_financial_types.py
+++ tests/unit/infrastructure/database/test_financial_types.py
@@ -1,10 +1,22 @@
+import pytest
+
 from ate_api.domain.financial_types import FinancialType
 from ate_api.infrastructure.database import FinancialTypeName
 
 
+@pytest.mark.parametrize(
+    "type_, type_name",
+    [
+        (FinancialType.EXPECTED_COST, FinancialTypeName.EXPECTED_COST),
+        (FinancialType.ACTUAL_COST, FinancialTypeName.ACTUAL_COST),
+        (FinancialType.FUNDING_ALLOCATION, FinancialTypeName.FUNDING_ALLOCATION),
+        (FinancialType.SPEND_TO_DATE, FinancialTypeName.SPEND_TO_DATE),
+        (FinancialType.FUNDING_REQUEST, FinancialTypeName.FUNDING_REQUEST),
+    ],
+)
 class TestFinancialTypeName:
-    def test_from_domain(self) -> None:
-        assert FinancialTypeName.from_domain(FinancialType.FUNDING_ALLOCATION) == FinancialTypeName.FUNDING_ALLOCATION
+    def test_from_domain(self, type_: FinancialType, type_name: FinancialTypeName) -> None:
+        assert FinancialTypeName.from_domain(type_) == type_name
 
-    def test_to_domain(self) -> None:
-        assert FinancialTypeName.FUNDING_ALLOCATION.to_domain() == FinancialType.FUNDING_ALLOCATION
+    def test_to_domain(self, type_: FinancialType, type_name: FinancialTypeName) -> None:
+        assert type_name.to_domain() == type_
--- tests/unit/routes/test_financial_types.py
+++ tests/unit/routes/test_financial_types.py
@@ -1,10 +1,22 @@
+import pytest
+
 from ate_api.domain.financial_types import FinancialType
 from ate_api.routes.financial_types import FinancialTypeModel
 
 
+@pytest.mark.parametrize(
+    "type_, type_model",
+    [
+        (FinancialType.EXPECTED_COST, FinancialTypeModel.EXPECTED_COST),
+        (FinancialType.ACTUAL_COST, FinancialTypeModel.ACTUAL_COST),
+        (FinancialType.FUNDING_ALLOCATION, FinancialTypeModel.FUNDING_ALLOCATION),
+        (FinancialType.SPEND_TO_DATE, FinancialTypeModel.SPEND_TO_DATE),
+        (FinancialType.FUNDING_REQUEST, FinancialTypeModel.FUNDING_REQUEST),
+    ],
+)
 class TestFinancialTypeModel:
-    def test_from_domain(self) -> None:
-        assert FinancialTypeModel.from_domain(FinancialType.FUNDING_ALLOCATION) == FinancialTypeModel.FUNDING_ALLOCATION
+    def test_from_domain(self, type_: FinancialType, type_model: FinancialTypeModel) -> None:
+        assert FinancialTypeModel.from_domain(type_) == type_model
 
-    def test_to_domain(self) -> None:
-        assert FinancialTypeModel.FUNDING_ALLOCATION.to_domain() == FinancialType.FUNDING_ALLOCATION
+    def test_to_domain(self, type_: FinancialType, type_model: FinancialTypeModel) -> None:
+        assert type_model.to_domain() == type_
