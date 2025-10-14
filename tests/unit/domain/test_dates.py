dadac7b916e30931a958f57f440c493e41ecd2a0
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-10-14 16:01:19 +0100

    Improve test method names

--- tests/unit/domain/test_dates.py
+++ tests/unit/domain/test_dates.py
@@ -6,12 +6,12 @@ from ate_api.domain.dates import DateTimeRange
 
 
 class TestDateTimeRange:
-    def test_create_closed(self) -> None:
+    def test_can_create_closed_range(self) -> None:
         date_time_range = DateTimeRange(datetime(2020, 1, 1), datetime(2020, 2, 1))
 
         assert date_time_range.from_ == datetime(2020, 1, 1) and date_time_range.to == datetime(2020, 2, 1)
 
-    def test_create_open(self) -> None:
+    def test_can_create_open_range(self) -> None:
         date_time_range = DateTimeRange(datetime(2020, 1, 1))
 
         assert date_time_range.from_ == datetime(2020, 1, 1) and not date_time_range.to
@@ -24,9 +24,9 @@ class TestDateTimeRange:
             (datetime(2020, 1, 1), None),
         ],
     )
-    def test_from_before_or_equal_to_to(self, from_: datetime, to: datetime | None) -> None:
+    def test_can_create_range_with_from_before_or_equal_to_to(self, from_: datetime, to: datetime | None) -> None:
         DateTimeRange(from_, to)
 
-    def test_from_after_to(self) -> None:
+    def test_cannot_create_range_with_from_after_to(self) -> None:
         with pytest.raises(ValueError, match="From '2020-01-01 12:00:00' must not be after to '2019-12-31 13:00:00'"):
             DateTimeRange(datetime(2020, 1, 1, 12), datetime(2019, 12, 31, 13))
