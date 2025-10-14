1a2cf8b27c972272586a632bd055bad6c467c9b2
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-10-14 15:57:48 +0100

    Correct variable name

--- tests/unit/domain/test_dates.py
+++ tests/unit/domain/test_dates.py
@@ -7,14 +7,14 @@ from ate_api.domain.dates import DateTimeRange
 
 class TestDateTimeRange:
     def test_create_closed(self) -> None:
-        date_range = DateTimeRange(datetime(2020, 1, 1), datetime(2020, 2, 1))
+        date_time_range = DateTimeRange(datetime(2020, 1, 1), datetime(2020, 2, 1))
 
-        assert date_range.from_ == datetime(2020, 1, 1) and date_range.to == datetime(2020, 2, 1)
+        assert date_time_range.from_ == datetime(2020, 1, 1) and date_time_range.to == datetime(2020, 2, 1)
 
     def test_create_open(self) -> None:
-        date_range = DateTimeRange(datetime(2020, 1, 1))
+        date_time_range = DateTimeRange(datetime(2020, 1, 1))
 
-        assert date_range.from_ == datetime(2020, 1, 1) and not date_range.to
+        assert date_time_range.from_ == datetime(2020, 1, 1) and not date_time_range.to
 
     @pytest.mark.parametrize(
         "from_, to",
