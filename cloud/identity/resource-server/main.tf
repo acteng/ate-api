8b4bffee49e969aaa29000098b13e49db3f9dfa6
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-12-03 11:57:03 +0000

    Remove resource rename now applied to Prod

--- cloud/identity/resource-server/main.tf
+++ cloud/identity/resource-server/main.tf
@@ -10,11 +10,6 @@ locals {
   minute_in_seconds = 60
 }
 
-moved {
-  from = auth0_resource_server.server
-  to   = auth0_resource_server.main
-}
-
 resource "auth0_resource_server" "main" {
   name           = "ATE API"
   identifier     = var.identifier
