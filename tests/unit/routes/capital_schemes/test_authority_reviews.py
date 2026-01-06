ac35effea4c0ceb1379ddbf6afa5805b98eb1651
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-01-06 10:35:18 +0000

    Correct authority review domain test class
    Inadvertently placed in the wrong test class in [1].
    
    [1] 172be8b0d31dd0eb98b7013bea69a64781527fc3

--- tests/unit/routes/capital_schemes/test_authority_reviews.py
+++ tests/unit/routes/capital_schemes/test_authority_reviews.py
@@ -1,21 +1,10 @@
 from datetime import UTC, datetime
 
-import pytest
-
 from ate_api.domain.capital_schemes.authority_reviews import CapitalSchemeAuthorityReview
 from ate_api.routes.capital_schemes.authority_reviews import CapitalSchemeAuthorityReviewModel
 
 
 class TestCapitalSchemeAuthorityReviewModel:
-    def test_can_create(self) -> None:
-        authority_review = CapitalSchemeAuthorityReview(review_date=datetime(2020, 1, 1, tzinfo=UTC))
-
-        assert authority_review.review_date == datetime(2020, 1, 1, tzinfo=UTC)
-
-    def test_cannot_create_with_local_review_date(self) -> None:
-        with pytest.raises(ValueError, match="Review date and time must include a time zone: 2020-01-01 00:00:00"):
-            CapitalSchemeAuthorityReview(review_date=datetime(2020, 1, 1))
-
     def test_from_domain(self) -> None:
         authority_review = CapitalSchemeAuthorityReview(review_date=datetime(2020, 1, 1, tzinfo=UTC))
 
