b0533da066ee3d0dd34d8f1aa11ede3e9c7ef110
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-02-02 10:27:29 +0000

    GH-52: Use custom domain for local OIDC configuration URL

--- ate_api/settings.py
+++ ate_api/settings.py
@@ -6,7 +6,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str = "postgresql+asyncpg://ate:password@localhost/ate"
     create_database_schema: bool = False
-    oidc_server_metadata_url: str = "https://ate-api-dev.uk.auth0.com/.well-known/openid-configuration"
+    oidc_server_metadata_url: str = (
+        "https://dev.identity.api.activetravelengland.gov.uk/.well-known/openid-configuration"
+    )
     resource_server_identifier: str = "https://dev.api.activetravelengland.gov.uk"
 
     model_config = SettingsConfigDict(frozen=True, env_file=".env")
