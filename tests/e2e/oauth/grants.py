8695fd51a7bb8d36ffb7a77bb9cf8ab9fcf59232
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-02-11 11:36:45 +0000

    GH-86: Change E2E stub authorisation server to private key JWT

--- tests/e2e/oauth/grants.py
+++ tests/e2e/oauth/grants.py
@@ -1,5 +1,7 @@
 from authlib.oauth2.rfc6749 import ClientCredentialsGrant
 
+from tests.e2e.oauth.jwts import PrivateKeyJwtClientAssertion
 
-class ClientSecretPostClientCredentialsGrant(ClientCredentialsGrant):  # type: ignore
-    TOKEN_ENDPOINT_AUTH_METHODS = ["client_secret_post"]
+
+class PrivateKeyJwtClientCredentialsGrant(ClientCredentialsGrant):  # type: ignore
+    TOKEN_ENDPOINT_AUTH_METHODS = [PrivateKeyJwtClientAssertion.CLIENT_AUTH_METHOD]
