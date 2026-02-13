da54ba1aff03743e876cafa6cf172572973ad0b8
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-02-13 12:23:57 +0000

    GH-91: Add examples for get authority's capital schemes endpoint

--- ate_api/routes/authorities/capital_schemes.py
+++ ate_api/routes/authorities/capital_schemes.py
@@ -1,7 +1,7 @@
 from typing import Annotated, Literal
 
-from fastapi import APIRouter, Depends, HTTPException, Query, Request
-from pydantic import AnyUrl
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, Request
+from pydantic import AnyUrl, ConfigDict
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_CONTENT
 
 from ate_api.domain.authorities import AuthorityAbbreviation, AuthorityRepository
@@ -20,6 +20,22 @@ from ate_api.routes.collections import CollectionModel
 router = APIRouter(prefix="/{abbreviation}/capital-schemes")
 
 
+class CapitalSchemesModel(CollectionModel[AnyUrl]):
+    model_config = ConfigDict(
+        json_schema_extra={
+            "examples": [
+                {
+                    "items": [
+                        "https://api.activetravelengland.gov.uk/capital-schemes/ATE000001",
+                        "https://api.activetravelengland.gov.uk/capital-schemes/ATE000002",
+                        "https://api.activetravelengland.gov.uk/capital-schemes/ATE000003",
+                    ]
+                }
+            ]
+        }
+    )
+
+
 @router.get(
     "/bid-submitting", summary="Get authority bid submitting capital schemes", responses={HTTP_404_NOT_FOUND: {}}
 )
@@ -28,11 +44,16 @@ async def get_authority_bid_submitting_capital_schemes(
     funding_programmes: Annotated[FundingProgrammeRepository, Depends(get_funding_programme_repository)],
     capital_schemes: Annotated[CapitalSchemeRepository, Depends(get_capital_scheme_repository)],
     request: Request,
-    abbreviation: str,
-    funding_programme_codes: Annotated[list[str] | None, Query(alias="funding-programme-code")] = None,
-    bid_status: Annotated[BidStatusModel | None, Query(alias="bid-status")] = None,
-    current_milestones: Annotated[list[MilestoneModel | Literal[""]] | None, Query(alias="current-milestone")] = None,
-) -> CollectionModel[AnyUrl]:
+    abbreviation: Annotated[str, Path(examples=["LIV"])],
+    funding_programme_codes: Annotated[
+        list[str] | None, Query(alias="funding-programme-code", examples=["ATF3"])
+    ] = None,
+    bid_status: Annotated[BidStatusModel | None, Query(alias="bid-status", examples=["funded"])] = None,
+    current_milestones: Annotated[
+        list[MilestoneModel | Literal[""]] | None,
+        Query(alias="current-milestone", examples=["detailed design completed"]),
+    ] = None,
+) -> CapitalSchemesModel:
     """
     Gets the capital schemes submitted by an authority.
     """
@@ -55,7 +76,7 @@ async def get_authority_bid_submitting_capital_schemes(
     capital_scheme_links = [
         AnyUrl(str(request.url_for("get_capital_scheme", reference=str(reference)))) for reference in references
     ]
-    return CollectionModel[AnyUrl](items=capital_scheme_links)
+    return CapitalSchemesModel(items=capital_scheme_links)
 
 
 def _to_domain(milestone: MilestoneModel | Literal[""]) -> Milestone | None:
