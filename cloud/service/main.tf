20ff99e009463f822812e34227ec089875a21e91
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-02-17 10:57:13 +0000

    Deploy 0.15.0 to Test and Prod

--- cloud/service/main.tf
+++ cloud/service/main.tf
@@ -22,13 +22,13 @@ locals {
       github_action_deploy = true
     }
     test = {
-      image_tag            = "0.15.0-rc1"
+      image_tag            = "0.15.0"
       keep_idle            = false
       domain               = "test.${local.domain}"
       github_action_deploy = false
     }
     prod = {
-      image_tag            = "0.14.0"
+      image_tag            = "0.15.0"
       keep_idle            = true
       domain               = local.domain
       github_action_deploy = false
